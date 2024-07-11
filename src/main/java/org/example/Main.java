5d07ac0b5006e91e8df71b7c25c0f21a0411e40d
MikVit <mikhailvitoshnev@hotmail.com>
2024-07-11 10:17:37 +0300

    Was created a user class that takes the id and name parameters and compares them. If the parameters are correct then the program passes the user through Boolean logic

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,34 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        User user1 = new User(1234, "Vito");
+        User user2 = new User(1234, "Vito");
+        User user3 = new User(1231, "Dana");
+        System.out.println(user1.isEqual(user2));
+        System.out.println(user2.isEqual(user3));
     }
 }
+
+class User {
+    int id;
+    String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEqual(User user) {
+        return id == user.getId() && name.equals(user.getName());
+    }
+}
+
